ed30c97e5318179adb1151974491c956889afcae
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-07-25 11:55:16 -0300

    Add transaction fields

--- picpay/models.py
+++ picpay/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from decimal import Decimal
 
 
-class Profile(models.Model):
+class Account(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='picpay_profile')
+        User, on_delete=models.CASCADE,)
     complete_name = models.CharField(max_length=50)
     document = models.CharField(max_length=100)
     document_type = models.CharField(max_length=4, choices=[
@@ -14,15 +15,28 @@ class Profile(models.Model):
     account_type = models.CharField(max_length=10, choices=[(
         'personal', 'Personal'), ('merchant', 'Merchant')])
     balance = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0.00)
+        max_digits=10, decimal_places=2, default=0.00, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def pay(self, value):
+        value = Decimal(value)  # Converte o valor para Decimal
+        self.balance -= value
+        self.save()
+
+    def receive(self, value):
+        value = Decimal(value)  # Converte o valor para Decimal
+        self.balance += value
+        self.save()
 
     def __str__(self):
         return f'id:{self.id} {self.user.email}'
 
 
 class Transaction(models.Model):
-    value = ''
-    sender = ''
-    receiver = ''
+    value = models.DecimalField(
+        max_digits=10, decimal_places=2, editable=False)
+    sender = models.ForeignKey(
+        Account, related_name='sent_transactions', on_delete=models.CASCADE, editable=False)
+    receiver = models.ForeignKey(
+        Account, related_name='received_transactions', on_delete=models.CASCADE, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
