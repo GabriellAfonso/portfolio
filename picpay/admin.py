f8b1a048c76372bb0c200e67f37dc93b75b70f2f
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-07-25 11:57:18 -0300

    Add Transaction to Admin Interface

--- picpay/admin.py
+++ picpay/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Account, Transaction
 
-admin.site.register(Profile)
+admin.site.register(Account)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ('value', 'sender', 'receiver', 'created_at')
+    list_display = ('value', 'sender', 'receiver', 'created_at')
+
+    def has_delete_permission(self, request, obj=None):
+        # Não permite a exclusão de objetos
+        return False
+
+    def has_add_permission(self, request):
+        # Não permite a adição de novos objetos
+        return False
+
+
+admin.site.register(Transaction, TransactionAdmin)
