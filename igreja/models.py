bf8081417137a2bbd0171bdb2f5e4e536a3382bd
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-07-30 10:12:41 -0300

    Add models

--- igreja/models.py
+++ igreja/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Music(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, null=True, blank=True)
+
+
+class Played(models.Model):
+    music = models.ForeignKey(Music)
+    tone = models.CharField(max_length=2)
+    position = models.IntegerField()
+    date = models.DateField()
