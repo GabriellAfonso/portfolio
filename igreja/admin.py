0f2c9a497e3894e7bc8063b9e49bc3246917b813
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-08-14 14:25:21 -0300

    add models to admin

--- igreja/admin.py
+++ igreja/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, Music, Played
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Music)
+admin.site.register(Played)
