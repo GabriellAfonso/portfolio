79f8c0b819a185a6f6af240905e6dfeaab62244b
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2024-08-14 14:25:42 -0300

    add urls

--- igreja/urls.py
+++ igreja/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
-from .views import Home
+from .views import Home, Tables, RegisterSundays
 
 
 app_name = 'igreja'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('tabelas', Tables.as_view(), name='tabela'),
+    path('registrar-domingo', RegisterSundays.as_view(), name='register_sundays'),
 ]
