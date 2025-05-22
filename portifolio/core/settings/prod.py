4f6f099af8259e77c1139bdb85c73310c071bb68
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-05-22 08:22:34 -0300

    chore: remove prod.py and add to .gitignore

--- portifolio/core/settings/prod.py
+++
@@ -1,9 +0,0 @@
-from .base import *
-import os
-
-TEMPLATE_DEBUG = False
-CORS_ALLOW_ALL_ORIGINS = False
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://gabrielafonso.com.br']
