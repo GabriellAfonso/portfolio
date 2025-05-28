0e58d26a9eabe88bbb847fae07436fa2b005f0e3
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-05-28 10:26:43 -0300

    refactor(picpay): rename internal TransactionValidator methods to follow private naming convention

--- portifolio/apps/picpay/validators/transaction_validator.py
+++ portifolio/apps/picpay/validators/transaction_validator.py
@@ -15,24 +15,24 @@ class TransactionValidator():
     def validate(self, data):
         sender = data['sender']
         receiver = data['receiver']
-        self.check_positive_value(data['value'])
-        self.check_not_self_transfer(sender, receiver)
-        self.check_balance_sufficient(sender, data['value'])
-        self.check_permissions(sender, receiver)
+        self._check_positive_value(data['value'])
+        self._check_not_self_transfer(sender, receiver)
+        self._check_balance_sufficient(sender, data['value'])
+        self._check_permissions(sender, receiver)
 
-    def check_positive_value(self, value):
+    def _check_positive_value(self, value):
         if value <= 0:
             raise ValidationError("O valor da transação deve ser positivo.")
 
-    def check_not_self_transfer(self, sender, receiver):
+    def _check_not_self_transfer(self, sender, receiver):
         if sender.id == receiver.id:
             raise SelfTransferError
 
-    def check_balance_sufficient(self, sender, value):
+    def _check_balance_sufficient(self, sender, value):
         if sender.balance < value:
             raise InsufficientBalanceError(sender)
 
-    def check_permissions(self, sender, receiver):
+    def _check_permissions(self, sender, receiver):
         if not has_permission(sender.user, 'make_transfer'):
             raise TransferPermissionDenied
         elif not has_permission(receiver.user, 'receive_transfer'):
