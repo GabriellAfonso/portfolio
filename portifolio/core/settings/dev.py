d04e68574dd21445560ad0f735ca1d2c9f90b0fe
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-08-19 15:45:58 -0300

    feat(settings): add dotenv configuration and environment variable loading

--- portifolio/core/settings/dev.py
+++ portifolio/core/settings/dev.py
@@ -1,5 +1,29 @@
 from .base import *
 import os
+from dotenv import load_dotenv
+dotenv_path = BASE_DIR / "dotenv_files/.env"
+load_dotenv(dotenv_path)
+
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
+
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
+
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+    if h.strip()
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
+        'NAME': os.getenv('POSTGRES_DB', 'change-me'),
+        'USER': os.getenv('POSTGRES_USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES_HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES_PORT', 'change-me'),
+    }
+}
 
 
 TEMPLATE_DEBUG = True
